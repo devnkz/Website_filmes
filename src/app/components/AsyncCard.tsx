aa3033dad4adf104369aeb18fa8655cb98fa9d1d
devnkz <vieiranyckolas1@gmail.com>
2025-05-18 13:16:12 -0300

    responsividade corrigida

--- src/app/components/AsyncCard.tsx
+++ src/app/components/AsyncCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
@@ -31,8 +33,8 @@ export function AsyncCard() {
                     <div
                         key={movie.id}
                         className="bg-zinc-800 p-4 flex flex-col justify-center items-center w-full h-[500px] md:w-2/5 xl:w-1/4 rounded-lg
-              hover:shadow-xl shadow-zinc-700 transition-all duration-500 relative overflow-hidden 
-              md:hover:-translate-y-3 group hidden md:block"
+                        hover:shadow-xl shadow-zinc-700 transition-all duration-500 relative overflow-hidden 
+                        md:hover:-translate-y-3 group block"
                         onClick={() => {
                             if (typeof window !== "undefined" && window.innerWidth < 768) {
                                 setOpenCardId(prev => (prev === movie.id ? null : movie.id));
@@ -52,11 +54,11 @@ export function AsyncCard() {
                         <p className="font-bold text-xl text-green-600 z-10">Nota: {movie.vote_average}</p>
 
                         <div className={`
-  absolute inset-0 bg-black bg-opacity-80 backdrop-blur-sm text-white p-4
-  flex flex-col justify-center items-start text-justify z-20 transition-all duration-500
-  ${isOpen ? 'opacity-90 translate-y-0' : 'opacity-0 translate-y-full hidden'}
-  md:group-hover:opacity-90 md:group-hover:translate-y-0 md:block
-`}>
+                            absolute inset-0 bg-black bg-opacity-80 backdrop-blur-sm text-white p-4
+                            flex flex-col justify-center items-start text-justify z-20 transition-all duration-500
+                            ${isOpen ? 'opacity-90 translate-y-0' : 'opacity-0 translate-y-full'}
+                            md:group-hover:opacity-90 md:group-hover:translate-y-0
+                        `}>
                             <span className="font-bold text-lg mb-2">Sinopse:</span>
                             <p className="text-sm">{movie.overview}</p>
                         </div>
